90875f617db950d72bd269eaa65384d1d4dc4017
DREAMweaver <apple@apples-MacBook-Pro.local>
2020-07-26 11:24:19 +0530

    if else conditions

---
+++ bin/ifelse.py
@@ -0,0 +1,9 @@
+a = int(input("enter a value :"))
+b = int(input("enter b value :"))
+c = int(input("enter b value :"))
+if a > b and a > c:
+    print("a is bigger than b and c")
+elif b > a and b > c:
+    print("b is bigger than a and c")
+else:
+    print("c is bigger than a and b")
