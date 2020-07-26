a79f1f53c72944161d984cc770350e260c697fb3
DREAMweaver <apple@apples-MacBook-Pro.local>
2020-07-26 12:01:18 +0530

    for loops

---
+++ bin/forloop.py
@@ -0,0 +1,15 @@
+x = ["gopi","chand",1,5,4]
+for i in x:
+    print(i)
+    if i == "chand":
+      break
+
+for x in range(10):
+    print(x)
+for i in range(1,15):
+    if(i%2==0):
+        print(i)
+for i in range(1,20,5):
+    print(i)
+
+
