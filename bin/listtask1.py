a0713af34331546e834d35e03980ce91472e5355
DREAMweaver <apple@apples-MacBook-Pro.local>
2020-07-27 13:15:28 +0530

    task1

---
+++ bin/listtask1.py
@@ -0,0 +1,31 @@
+# city_name = ['Bangalore ', 'Kolkata ', 'Chennai', 'Delhi', 'Jammu']
+# print(type(city_name),"length of city_name is" ,len(city_name))
+# city_name.remove("Delhi")
+# print(city_name)
+# for x in city_name:
+#   if "indore" in x:
+#       print("yes indore exists in list ")
+#   else:
+#       print("no indore is not in the list")
+#       break
+#
+# city_name.extend(["Kanpur","Chandigarh"])
+# print(city_name)
+# # for x in city_name:
+# #     print(x)
+a = [1,2,3,4,5,6,7,8,9,10]
+even = []
+odd = []
+for i in  a:
+    if ( i % 2 == 0):
+        even.append(i)
+    else:
+        odd.append(i)
+print("even_list" , even)
+print("odd_list" , odd)
+dict = dict(zip(odd, even))
+print(dict)
+
+
+
+
