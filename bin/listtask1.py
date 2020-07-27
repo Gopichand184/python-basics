cdc65f106fe469b6e6ad6aea97c424ec22424ba8
DREAMweaver <apple@apples-MacBook-Pro.local>
2020-07-27 18:24:30 +0530

    listtask1

--- bin/listtask1.py
+++ bin/listtask1.py
@@ -1,18 +1,18 @@
-# city_name = ['Bangalore ', 'Kolkata ', 'Chennai', 'Delhi', 'Jammu']
-# print(type(city_name),"length of city_name is" ,len(city_name))
-# city_name.remove("Delhi")
-# print(city_name)
+city_name = ['Bangalore ', 'Kolkata ', 'Chennai', 'Delhi', 'Jammu']
+print(type(city_name),"length of city_name is" ,len(city_name))
+city_name.remove("Delhi")
+print(city_name)
+for x in city_name:
+  if "indore" in x:
+      print("yes indore exists in list ")
+  else:
+      print("no indore is not in the list")
+      break
+
+city_name.extend(["Kanpur","Chandigarh"])
+print(city_name)
 # for x in city_name:
-#   if "indore" in x:
-#       print("yes indore exists in list ")
-#   else:
-#       print("no indore is not in the list")
-#       break
-#
-# city_name.extend(["Kanpur","Chandigarh"])
-# print(city_name)
-# # for x in city_name:
-# #     print(x)
+#     print(x)
 a = [1,2,3,4,5,6,7,8,9,10]
 even = []
 odd = []
@@ -25,6 +25,24 @@ print("even_list" , even)
 print("odd_list" , odd)
 dict = dict(zip(odd, even))
 print(dict)
+print(dict.keys())
+print(dict.values())
+a =['abc', 'xyz', 'aba', '1221', '2342','samples']
+total_length = len(a)
+for i in a:
+    print(len(i))
+      if len(i) > 1 and i[0] == i[-1]:
+          print(i)
+
+print(total_length)
+
+
+
+
+
+
+
+
 
 
 
---
+++ bin/listtask2.py
@@ -0,0 +1,19 @@
+a = [10,20,30,20,10,50,60,40,80,50,40]
+a = list(dict.fromkeys(a))
+print(a)
+color = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+color.pop(0)
+color.pop(4)
+color.pop(3)
+for i in color:
+    print(i)
+list1 = [1, 3, 5, 7, 9]
+list2 = [1, 2, 4, 6, 7, 8]
+list3 = (list(set(list1) - set(list2)))
+print(list3)
+list1 = [1, 2, 3, 0]
+list2 = ['Red', 'Green', 'Black']
+list1.append(list2)
+print(list1)
+
+
