1e47cd285a2f2b88c427bcdf0e7d0c000c6c1ef6
DREAMweaver <apple@apples-MacBook-Pro.local>
2020-07-28 11:56:01 +0530

    functions

---
+++ bin/functions.py
@@ -0,0 +1,16 @@
+def num_range(num):
+    for i in range(num):
+        print(i)
+
+num_range(100)
+num_range(50)
+def add(num1,num2):
+    sum = num1+num2
+    return sum
+
+def div(num1,num2):
+    sum = add(num1,num2)
+    res = (sum)/2
+    return res
+res = div(10,10)
+print(res)
